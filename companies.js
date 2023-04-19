2ccaa1738c0d795ebd5bfde18ec6286d237fb299
angelicahorngren <94979102+angelicahorngren@users.noreply.github.com>
2023-04-19 16:23:05 +0200

    jamal picture everywhere

--- companies.js
+++ companies.js
@@ -1,11 +1,11 @@
 const { Company } = require('./Company.py');
 
 const companies = [
-    new Company("GreenTech Co.", 8.5, 7.9, 9.1, 6.8, "green_tech_description.txt", "green_tech_picture.png"),
-    new Company("EcoLife Inc.", 7.2, 9.4, 6.5, 8.1, "eco_life_description.txt", "eco_life_picture.png"),
-    new Company("SustainCorp LLC", 9.1, 8.3, 7.8, 7.2, "sustaincorp_description.txt", "sustaincorp_picture.png"),
-    new Company("PlanetFirst Enterprises", 8.3, 7.5, 8.7, 6.9, "planetfirst_description.txt", "planetfirst_picture.png"),
-    new Company("NatureWorks Co.", 9.0, 9.2, 6.9, 7.8, "natureworks_description.txt", "natureworks_picture.png"),
-    new Company("EnviroGreen Solutions", 7.9, 8.5, 8.2, 7.1, "envirogreen_description.txt", "envirogreen_picture.png")
+    new Company("GreenTech Co.", 8.5, 7.9, 9.1, 6.8, "test_description.txt", "jamal.jpeg"),
+    new Company("EcoLife Inc.", 7.2, 9.4, 6.5, 8.1, "test_description.txt", "jamal.jpeg"),
+    new Company("SustainCorp LLC", 9.1, 8.3, 7.8, 7.2, "test_description.txt", "jamal.jpeg"),
+    new Company("PlanetFirst Enterprises", 8.3, 7.5, 8.7, 6.9, "test_description.txt", "jamal.jpeg"),
+    new Company("NatureWorks Co.", 9.0, 9.2, 6.9, 7.8, "test_description.txt", "jamal.jpeg"),
+    new Company("EnviroGreen Solutions", 7.9, 8.5, 8.2, 7.1, "test_description.txt", "jamal.jpeg")
   ];
   
